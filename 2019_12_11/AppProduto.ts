8b5f5fd82d0c1936e6dd50169ae755b2c2452808
danisacramento <danielleacsacramento@gmail.com>
2019-12-11 10:46:58 -0300

    exercícios TypeScript

---
+++ 2019_12_11/AppProduto.ts
@@ -0,0 +1,6 @@
+import {Produto} from "./Produtos";
+
+let p1: Produto;
+let p2: Produto;
+p1: new Produto(123, "livro","capa dura", 100.00, 10, "htt://fotodolivro.com.br/foto.jpg");
+p2: new Produto(158, "caderno","brochura", 10.00, 5,"htt://fotodocaderno.com.br/foto.jpg");
